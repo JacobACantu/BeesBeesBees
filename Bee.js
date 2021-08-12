d4fa2037dde438cab656ae1353ac24d82f2978d5
Jacob Cantu <85659554+JacobACantu@users.noreply.github.com>
2021-08-12 14:50:43 -0500

    Add files via upload

---
+++ Bee.js
@@ -0,0 +1,9 @@
+var Bee = function(age, color, food) {
+    Grub.call(this, age, color, food)
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'keep on growing';
+};
+
+Bee.prototype = Object.create(Grub.prototype)
+Bee.prototype.constructor = Bee
